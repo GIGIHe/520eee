d6d28d69f4abb27358fbbb884332385100fc90d9
GIGIHe <2697190181@qq.com>
2020-05-20 15:08:46 +0800

    1212

---
+++ js/data.js
@@ -0,0 +1,186 @@
+var htmls0 = '\
+						 	 	 <li class="ans_item">\
+								  <p class="ans_title" qus="1">9月26日，中共中央办公厅、国务院办公厅、中央军委办公厅印发______。</p>\
+								  <ul class="qus_list">\
+								    <li>A.《烈士纪念设施规划建设修缮管理维护总体工作方案》</li>\
+								    <li>B. 《烈士公祭办法》</li>\
+								    <li>C.《烈士公祭条例》</li>\
+								    <li>D.《烈士纪念设施维修管理方案》</li>\
+								  </ul>\
+								</li>\
+								<li class="ans_item">\
+								  <p class="ans_title" qus="1">2019年9月1日讯，生态环境部宣布,从今天开始,利用_______时间开展对群众反映强烈的生态环境问题平时不作为、急时“一刀切”问题进行专项整治。</p>\
+								  <ul class="qus_list">\
+								    <li>A.3个月</li>\
+								    <li>B.4个月</li>\
+								    <li>C.半年</li>\
+								    <li>D.一年</li>\
+								  </ul>\
+								</li>\
+								<li class="ans_item">\
+								  <p class="ans_title"qus="1">2019年9月4日电，卫星导航应用的蓬勃发展，始终伴随航天技术的创新与突破。北斗卫星导航系统是全球四大卫星导航核心供应商之一，目前在轨卫星已达_______颗。</p>\
+								  <ul class="qus_list">\
+								    <li>A.39</li>\
+								    <li>B.40</li>\
+								    <li>C.45</li>\
+								    <li>D.46</li>\
+								  </ul>\
+								</li>\
+								<li class="ans_item">\
+								  <p class="ans_title"qus="4">王某未经许可，私自盗印某作家畅销小说10万册，获利60万元。王某的行为构成( )</p>\
+								  <ul class="qus_list">\
+								    <li>A.销售盗版罪</li>\
+								    <li>B.非法经营罪</li>\
+								    <li>C.非法出版罪</li>\
+								    <li>D.侵犯著作权罪</li>\
+								  </ul>\
+								</li>\
+								<li class="ans_item">\
+								  <p class="ans_title"qus="1">2019年9月20日，习主席在河南主持召开黄河流域生态保护和高质量发展座谈会时强调:共同抓好大保护协同推进大治理，让黄河成为造福人民的_____。</p>\
+								  <ul class="qus_list">\
+								    <li>A.幸福河</li>\
+								    <li>B.母亲河</li>\
+								    <li>C.第一大河</li>\
+								    <li>D.第二大河</li>\
+								  </ul>\
+								</li>\
+								<li class="ans_item">\
+								  <p class="ans_title"qus="2">某市环保局对某一污染严重的工厂作出责令停业决定，在性质上属于:</p>\
+								  <ul class="qus_list">\
+								    <li>A.行政强制措施</li>\
+								    <li>B.行政处罚</li>\
+								    <li>C.行政强制执行</li>\
+								    <li>D.行政制裁</li>\
+								  </ul>\
+								</li>\
+								<li class="ans_item">\
+								<p class="ans_title"qus="2">某企业非法集资，被处以资金冻结。这属于：</p>\
+								<ul class="qus_list">\
+								  <li>A.财产罚</li>\
+								  <li>B.行政强制措施</li>\
+								  <li>C.申诫罚</li>\
+								  <li>D.能力罚</li>\
+								</ul>\
+							  </li>\
+								<li class="ans_item">\
+								  <p class="ans_title"qus="2">下列不属于元曲四大悲剧的是()。</p>\
+								  <ul class="qus_list">\
+								    <li>A.《窦娥冤》</li>\
+								    <li>B.《西厢记》</li>\
+								    <li>C.《梧桐雨》</li>\
+								    <li>D.《赵氏孤儿》</li>\
+								  </ul>\
+								</li>\
+								<li class="ans_item">\
+								  <p class="ans_title"qus="4">法立而不行，与无法等。这句话强调了：</p>\
+								  <ul class="qus_list">\
+								    <li>A. 立法的重要性</li>\
+								    <li>B. 学法的重要性</li>\
+								    <li>C. 知法的重要性</li>\
+								    <li>D. 守法的重要性</li>\
+								  </ul>\
+								</li>\
+								<li class="ans_item">\
+								  <p class="ans_title"qus="1">10.交通灯的颜色由上至下的排列是( )</p>\
+								  <ul class="qus_list">\
+								    <li>A.红、黄、绿</li>\
+								    <li>B.红、绿、黄</li>\
+								    <li>C.绿、红、黄</li>\
+								    <li>D.绿、黄、红</li>\
+								  </ul>\
+								</li>';
+
+var htmls1 = '\
+					<li class="ans_item">\
+						<p class="ans_title" qus="1">沸腾：沸点</p>\
+						<ul class="qus_list">\
+						<li>A.航行：动力</li>\
+						<li>B.取暖：点火</li>\
+						<li>C.加热：融化</li>\
+						<li>D.联络：书信</li>\
+					</ul>\
+					</li>\
+					<li class="ans_item">\
+						<p class="ans_title" qus="1">你做一项工作，只是为了谋生，对它并不喜欢，这项工作就只是你的职业。你做一项工作，只是因为喜欢，并不在乎它能否带来利益，这项工作就是你的事业。最理想的情形是，事业和职业一致，做喜欢的事并能以此谋生。 这段文字主要叙述的是：</p>\
+						<ul class="qus_list">\
+						<li>A.职业与事业 </li>\
+						<li>B.谋生与理想</li>\
+						<li>C.兴趣与工作</li>\
+						<li>D.目标与现实</li>\
+					</ul>\
+					</li>\
+					<li class="ans_item">\
+						<p class="ans_title"qus="2">两名运动员进行110米栏赛跑，结果甲领先乙11米到达终点。同样乙与丙进行110米栏赛跑，结果乙领先丙11米到达终点。如果让甲与丙进行110米栏赛跑，那么甲到终点时，丙跑了多少?</p>\
+						<ul class="qus_list">\
+						<li>A.88米</li>\
+						<li> B.89米 </li>\
+						<li>C.90米</li>\
+						<li>D.91米</li>\
+					</ul>\
+					</li>\
+					<li class="ans_item">\
+						<p class="ans_title"qus="3">对于如何维持在孩子面前的权威性有很多不同观点，这使得父母有点儿________。填入划横线部分最恰当的一项是：</p>\
+						<ul class="qus_list">\
+						<li>A.独断专行</li>\
+						<li>B.不知所措</li>\
+						<li>C.无所适从</li>\
+						<li>D.顾此失彼</li>\
+					</ul>\
+					</li>\
+					<li class="ans_item">\
+						<p class="ans_title"qus="3"> 4，5，12，39，160，( )</p>\
+						<ul class="qus_list">\
+						<li>A.605</li>\
+						<li>B.705</li>\
+						<li>C.805</li>\
+						<li>D.90</li>\
+					</ul>\
+					</li>\
+					<li class="ans_item">\
+						<p class="ans_title"qus="4">黑匣子：飞机</p>\
+						<ul class="qus_list">\
+						<li>A.防护栏：工厂</li>\
+						<li>B.集装箱：货轮</li>\
+						<li>C.超新星：太空  </li>\
+						<li>D.记录仪：汽车</li>\
+					</ul>\
+					</li>\
+					<li class="ans_item">\
+						<p class="ans_title"qus="4">蝴蝶不是生下来就那么美丽的，只有经过不断的蠕行，日晒雨淋，它才能由一条奇丑无比的虫子，一点儿一点儿地蜕化成一只美丽的蝴蝶。这句话表达的观点是：</p>\
+						<ul class="qus_list">\
+						<li>A.美丽的事物都不是天生的</li>\
+						<li>B.任何时候都不能以貌取人</li>\
+						<li>C.美与丑是可以相互转化的</li>\
+						<li>D.成长需要经过苦难的磨砺</li>\
+					</ul>\
+					</li>\
+					<li class="ans_item">\
+						<p class="ans_title"qus="1">某种商品因换季准备打折出售，如果按定价的七五折销售将赔25元，如果按定价的九折出售将赚20元。则这种商品的定价是( )元。</p>\
+						<ul class="qus_list">\
+						<li>A.300</li>\
+						<li>B.200</li>\
+						<li>C.100</li>\
+						<li>D.250</li>\
+					</ul>\
+					</li>\
+					<li class="ans_item">\
+						<p class="ans_title"qus="2">据国外媒体________，植物长期以来一直被认为只会保持________。但事实证明，他们一直在“说话”。填入划横线部分最恰当的一项是：</p>\
+						<ul class="qus_list">\
+						<li>A.透漏 缄默</li>\
+						<li>  B.报道 沉默</li>\
+						<li>C.揭露 沉寂</li>\
+						<li>  D.报告 安静</li>\
+					</ul>\
+					</li>\
+					<li class="ans_item">\
+						<p class="ans_title"qus="3">这种绝对平均主义虽然充满着诱人的理想色彩，又有反对封建农奴制的革命性质，但归根到底，是不可能实现的________。填入划横线部分最恰当的一项是：</p>\
+						<ul class="qus_list">\
+						<li>A.虚无缥缈 </li>\
+						<li>B.过眼云烟</li>\
+						<li>C.空中楼阁</li>\
+						<li>D.无源之水</li>\
+					</ul>\
+					</li>';
+
+var img1 = '<img src="images/jk1.jpg" alt="">'
+var img2 = '<img src="images/gk1.jpg" alt="">'
